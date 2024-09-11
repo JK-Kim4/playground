98204d6ec5c47a76fa970e49bd3cd9fb62864704
jongwan <jongbell4@gmail.com>
2024-09-11 10:38:38 +0900

    golang map

--- go/algorithm-go/graph/main.go
+++ go/algorithm-go/graph/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
-	queue "graph/packages/queue"
 )
 
 func main() {
 
-	queue := &queue.Queue{};
-	fmt.Print(queue.IsEmpty())
-	
+	graphMap := map[string][]string{
+		"A": {"B", "D", "E"},
+		"B": {"A", "C", "D"},
+		"C": {"B"},
+		"D": {"A", "B"},
+		"E": {"A"},
+	}
+
+	bfs(graphMap, "A")
+
+}
+
+func bfs(graph map[string][]string, startV string) {
+
+	fmt.Println(graph)
+	fmt.Println(startV)
+
+	fmt.Println(graph[startV])
 
 }

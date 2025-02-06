7ef0a913744f7d5ec2e24e34a56739cb8ba4584b
jongwan <jongbell4@gmail.com>
2025-02-07 00:14:55 +0900

    feat: Bean 객체는 단 하나만 생성되어 사용(싱글톤)

--- spring/src/main/java/com/jongwan/spring/AppContext.java
+++ spring/src/main/java/com/jongwan/spring/AppContext.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppContext {
 
-    @Bean(name = "greeter")
+    @Bean(name = "gtr")
     public Greeter greeter(){
         Greeter greeter = new Greeter();
         greeter.setFormat("%s 안녕하세요!");
--- spring/src/main/java/com/jongwan/spring/Main.java
+++ spring/src/main/java/com/jongwan/spring/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
 
-        Greeter g = ctx.getBean("greeter", Greeter.class);
-        String msg = g.greeting("스프링");
-        System.out.println(msg);
+        Greeter g1 = ctx.getBean("gtr", Greeter.class);
+        Greeter g2 = ctx.getBean("gtr", Greeter.class);
+        System.out.println("(g1 == g2): " + (g1 == g2));
+        System.out.println("(g1 == g2): " + g1.equals(g2));
         ctx.close();
 
     }

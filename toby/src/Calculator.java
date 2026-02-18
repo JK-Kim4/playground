36f9ac0344917eea77c2e4ce47d7b072eeb1dad5
jongwan <jongbell4@gmail.com>
2026-02-19 02:02:32 +0900

    템플릿 interface 분리 리펙토링 2

--- toby/src/Calculator.java
+++ toby/src/Calculator.java
@@ -1,8 +1,35 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
+
+    public Integer lineReadTemplate(String filePath, LineCallBack callBack, int initVal)
+        throws IOException {
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            int res = initVal;
+            String line;
+            while ((line = br.readLine()) != null) {
+                res = callBack.doSomethingWithLine(line, res);
+            }
+            return res;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
 
     public Integer fileReadTemplate(String filePath, BufferedReaderCallback callback)
         throws IOException {
@@ -28,7 +55,7 @@ public class Calculator {
     }
 
     public Integer calcSum(String filePath) throws IOException {
-        BufferedReaderCallback sumCallBack =
+        /*BufferedReaderCallback sumCallBack =
             br -> {
                 int sum = 0;
                 String line = null;
@@ -38,11 +65,15 @@ public class Calculator {
                 return sum;
             };
 
-        return fileReadTemplate(filePath, sumCallBack);
+        return fileReadTemplate(filePath, sumCallBack);*/
+
+        LineCallBack callBack =
+            (line, value) -> value + Integer.parseInt(line);
+        return lineReadTemplate(filePath, callBack, 0);
     }
 
     public Integer calcMultiple(String filePath) throws IOException {
-        BufferedReaderCallback sumCallBack =
+        /*BufferedReaderCallback sumCallBack =
             br -> {
                 int multiply = 1;
                 String line = null;
@@ -52,6 +83,9 @@ public class Calculator {
                 return multiply;
             };
 
-        return fileReadTemplate(filePath, sumCallBack);
+        return fileReadTemplate(filePath, sumCallBack);*/
+        LineCallBack callBack =
+            (line, value) -> value * Integer.parseInt(line);
+        return lineReadTemplate(filePath, callBack, 1);
     }
 }
---
+++ toby/src/LineCallBack.java
@@ -0,0 +1,4 @@
+public interface LineCallBack {
+    Integer doSomethingWithLine(String line, Integer value);
+
+}
